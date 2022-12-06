4fa05fa64e7a613beec4a1a0fa5b727b007311e5
AlinaVolkova18 <93998755+AlinaVolkova18@users.noreply.github.com>
2022-12-06 20:23:23 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_Store
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            Profile p = new Profile();
+            await Navigation.PushAsync(p);
+        }
+    }
+}
